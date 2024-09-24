f3cd02dca51483efbcb8f1c723c772df823dedff
rootaditiya <lifehealt016@gmail.com>
2024-09-24 07:10:40 +0700

    add carraousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,9 +1,56 @@
+import React, {useState} from 'react'
+import QuoteImage from '../assets/quote.svg'
 
-export default function Carousel({children: slides, key}){
+export default function Carousel({slides}){
+
+	const [currentIndex, setCurrentIndex] = useState(-1);
+
+  	const extendedSlides = [slides[slides.length - 1], ...slides, slides[0]];
+
+	const handlePrev = () => {
+	    setCurrentIndex((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
+
+	};
+
+	const handleNext = () => {
+    	setCurrentIndex((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+	};
+
+	const handleView = (index) => {
+		return index >= extendedSlides.length ? 1 : index +1 ;
+	}
+
+	const getTranslateValue = () => {
+	    return `-${currentIndex * 100 + 50}%`;
+	  };
+
 	return(
-		<div className="relative lg:overflow-auto md:overflow-hidden" key={key}>
-			<div className="flex">		
-				{slides}
+		<div className="relative flex flex-col space-y-5 justify-center items-center lg:overflow-auto md:overflow-hidden">
+			<div className="relatif flex overflow-hidden max-w-[600px] items-center py-5 space-y-5 "
+			>		
+				
+				{
+					extendedSlides.map((item, index) => (
+						<div className={`${handleView(currentIndex) === index ? 'scale-100': 'scale-75'} -left-50 min-w-[300px] relative flex flex-col shadow-xl shadow-appskep-primary/25 justify-center items-center w-1/3 ${handleView(currentIndex) === index ? 'opacity-100': 'opacity-50'} transition-transform duration-500 ease-in-out`} key={index}
+							style={{ 
+								  transform: `translateX(${getTranslateValue()}) scale(${handleView(currentIndex) ? '1,1' : '0.5,0.5'})` 
+								}}
+
+						>
+							<div className="absolute right-0 top-0 max-w-[50px] ">
+								<img alt="" src={QuoteImage}/>
+							</div>
+							<div className="block h-[100px]">
+								
+							</div>
+							<div className="px-5 py-10">{item.comment}</div>
+						</div>
+						))
+				}
+			</div>
+			<div className="flex flex-row space-x-10 items-center">
+				<button onClick={handlePrev} className="bg-slate-100 rounded-xl px-2 py-1"><h2>{"<"}</h2></button>
+				<button onClick={handleNext} className="bg-slate-100 rounded-xl px-2 py-1"><h2>{">"}</h2></button>
 			</div>
 		</div>
 		);
--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,7 +1,5 @@
 import Section from './Section.js'
 import Carousel from './Carousel.js'
-import QuoteImage from '../assets/quote.svg'
-
 
 export default function Feedback(){
 
@@ -20,21 +18,7 @@ export default function Feedback(){
 					</p>
 
 					<div className="max-w-screen-lg">
-						<Carousel>
-							{
-								slides.map((item) => (
-									<div className="relative flex flex-col items-center m-5 p-5 min-w-[300px]" key={item.id}>
-										<div className="absolute right-0 top-0 max-w-[50px] ">
-											<img alt="" src={QuoteImage}/>
-										</div>
-										<div className="block h-[100px] bg-appskep-primary">
-											
-										</div>
-										<div className="px-5 py-10">{item.comment}</div>
-									</div>
-									))
-							}
-						</Carousel>
+						<Carousel slides={slides}/>
 					</div>
 
 				</div>
